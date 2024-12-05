0c11855ef11b70bdcb53ab0f13cec91e44ed2d80
giovanna24dias <gi.gidcsilva@gmail.com>
2024-12-05 09:47:24 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,10 @@
+print('Bom dia')
+nome_usuario='giovanna dias' # tipo -> str=string
+idade_usuario=24 # tipo->int=integer
+altura_usuario=6.65 # tipo->float=ponto flutuante
+maior_idade=False # tipo _> bool=boolean
+
+print('seja bem-vidndo',nome_usuario)
+print('sua idade é',idade_usuario, 'anos')
+print('você tem ', altura_usuario, 'metros.')
+print('voê é maior de idade?',maior_idade)
